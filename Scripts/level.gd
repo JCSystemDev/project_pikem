c66ff1ca6f3bfe8b5e37283778c074ee606e0fe1
JC System <juancarlosgallardo87@gmail.com>
2024-11-11 22:20:47 -0300

    Traspaso de repositorio

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,12 +2,17 @@ extends Node2D
 
 @onready var left_door = $"Background/Doors/L Door"
 @onready var right_door = $"Background/Doors/R Door"
+var open: bool = false
 
 func _input(event):
 	if event.is_action_pressed("Start"):
-		print("ABRIR PUERTAS")
 		var tween = get_tree().create_tween()
 		var tween2 = get_tree().create_tween()
-		tween.tween_property(left_door, "position", Vector2(60, -213), 0.5)
-		tween2.tween_property(right_door, "position", Vector2(913, -213), 0.5)
-		
+		if !open:
+			open = true
+			tween.tween_property(left_door, "position", Vector2(60, -213), 0.5)
+			tween2.tween_property(right_door, "position", Vector2(913, -213), 0.5)
+		else:
+			tween.tween_property(left_door, "position", Vector2(339, -213), 0.5)
+			tween2.tween_property(right_door, "position", Vector2(634, -213), 0.5)
+			open = false
--- Scripts/player_pixel.gd
+++ Scripts/player_pixel.gd
@@ -1,44 +1,59 @@
 extends CharacterBody2D
 
-const SPEED = 5
+const speed = 300
 @onready var anim = $AnimationPlayer
 var meow: bool
-var direction = Vector2()
-
 func _ready():
+	
 	meow = false
 	
-func _physics_process(_delta):
-	if !meow:
-		if(Input.is_action_pressed("up") and direction.x == 0):
-			direction.y = -SPEED
-		elif(Input.is_action_pressed("down") and direction.x == 0):
-			direction.y = SPEED
-			anim.play("move_down")
-		elif(Input.is_action_pressed("left") and direction.y == 0):
-			direction.x = -SPEED
-		elif(Input.is_action_pressed("right") and direction.y == 0):
-			direction.x = SPEED
-		elif(Input.is_action_just_released("up")):
-			direction = Vector2.ZERO	
-		elif(Input.is_action_just_released("down")):
-			direction = Vector2.ZERO
-		elif(Input.is_action_just_released("left")):
-			direction = Vector2.ZERO
-		elif(Input.is_action_just_released("right")):
-			direction = Vector2.ZERO
+func _physics_process(delta):
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	if !meow:	
+		if direction:
+			velocity.x = speed * direction.x
+			velocity.y = speed * direction.y
 		else:
-			anim.play("idle")
-			direction = Vector2.ZERO
-			
-		move_and_collide(direction)
-	_update_animations()
+			velocity.x = 0
+			velocity.y = 0
+		
+		update_animations()
+	
+	move_and_slide()
+	
 
-func _update_animations():
+func update_animations():
 	if Input.is_action_just_pressed("ui_accept") and !meow:
 		meow = true
 	
 	if meow:
+		velocity = Vector2.ZERO
 		anim.play("meow")
 		await anim.animation_finished
 		meow = false
+	
+	if Input.is_action_just_pressed("down"):
+		anim.play("move_down")
+	
+	if Input.is_action_just_released("down") and velocity == Vector2.ZERO:
+		anim.play("idle_down")
+		
+	if Input.is_action_just_pressed("up"):
+		anim.play("move_up")
+	
+	if Input.is_action_just_released("up") and velocity == Vector2.ZERO:
+		anim.play("idle_up")
+		
+	if Input.is_action_just_pressed("left"):
+		anim.play("move_left")
+	
+	if Input.is_action_just_released("left") and velocity == Vector2.ZERO:
+		anim.play("idle_left")
+		
+	if Input.is_action_just_pressed("right"):
+		anim.play("move_right")
+	
+	if Input.is_action_just_released("right") and velocity == Vector2.ZERO:
+		anim.play("idle_right")
+
+	
